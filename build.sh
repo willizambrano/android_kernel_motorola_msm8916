09c19050f896df2bfc4cd1db9ac00d0563af9fde
willizambrano <zambranowilliam21@gmail.com>
2017-03-10 09:53:41 -0400

    :v

--- build.sh
+++ build.sh
@@ -12,20 +12,21 @@ nocol='\033[0m'
 echo -e "Borrando basura"
 make clean && make mrproper
 
-export CROSS_COMPILE=/home/william/linaro4.9/bin/arm-eabi-
+export CROSS_COMPILE=/home/william/linaro6.3/bin/arm-eabi-
 export ARCH=arm
+export SUBARCH=arm
 export KBUILD_BUILD_USER="WilliamZambrano"
 export KBUILD_BUILD_HOST="AMDfx6300"
 
-make osprey_defconfig
+make harpia_defconfig
 make menuconfig
 echo -e "$yellow*****************************************************"
 echo "       Compilando Evolution_Kernel         "
 echo -e "*****************************************************$nocol"
 
-make -o3 -j6 zImage CONFIG_DEBUG_SECTION_MISMATCH=y CONFIG_NO_ERROR_ON_MISMATCH=y
-make -o3 -j6 zImage-dtb CONFIG_DEBUG_SECTION_MISMATCH=y CONFIG_NO_ERROR_ON_MISMATCH=y
-make -j6 modules CONFIG_DEBUG_SECTION_MISMATCH=y CONFIG_NO_ERROR_ON_MISMATCH=y
+make -o3 -j6 zImage
+make -o3 -j6 dtimage
+make -j6 modules
 make -j6 modules_install INSTALL_MOD_PATH=evolution_install INSTALL_MOD_STRIP=1
 
 BUILD_END=$(date +"%s")
